ab07ff7d5394995e9e905c67ee9a249d90491a9d
Fernando <fernandogfleite@gmail.com>
2021-05-10 16:52:04 -0300

    Adição de mensagem de erro devido ao nome do produto em branco

--- mercadinho/produtos/custom_handlers.py
+++ mercadinho/produtos/custom_handlers.py
@@ -5,11 +5,13 @@ def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
     if response is not None:
         response.data['status_code'] = response.status_code
-        if response.data['status_code'] == 400 and response.data['id_category'][0].startswith("Invalid pk"):
-            return Response({"error": "Categoria não existente"}, status=400)
-        else:
+        try:
+            if response.data['status_code'] == 400 and response.data['id_category'][0].startswith("Invalid pk"):
+                return Response({"error": "Categoria não existente"}, status=400)
+            else:
+                return response
+        except:
             return response
-
     exc_list = str(exc).split("DETAIL: ")
-    
-    return Response({"error":exc_list[-1]})
+
+    return Response({"error":exc_list[-1]}, status=400)
--- mercadinho/produtos/models.py
+++ mercadinho/produtos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Produto(models.Model):
          
     created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100, blank=False)
+    name = models.CharField(max_length=100, blank=False, default="")
     description = models.CharField(max_length=100, blank=False, default="")
     value = models.FloatField()
     id_category = models.ForeignKey('Categoria', related_name='produtos_idcategory_categorias', on_delete= models.PROTECT)
@@ -13,6 +13,8 @@ class Produto(models.Model):
     def save(self, *args, **kwargs): 
         if self.value == 0:
             raise ValueError("O valor do produto não pode ser zero")
+        if self.name == "":
+            raise Exception("Produto sem nome")    
         if self.description == "":
             self.description = self.name
         self.category = {
